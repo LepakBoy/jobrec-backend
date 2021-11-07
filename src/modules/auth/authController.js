3f8843248d10e4b62e76ed59cb2131e1920e393c
LepakBoy <fikrinadzif35@gmail.com>
2021-11-07 14:39:35 +0700

    login, logout worker

--- src/modules/auth/authController.js
+++ src/modules/auth/authController.js
@@ -4,6 +4,7 @@ const helperWrapper = require("../../helper/");
 const authModel = require("./authModel");
 const bcrypt = require("bcrypt");
 const sendMail = require("../../helper/email");
+require("dotenv");
 
 module.exports = {
   registerWorker: async (req, res) => {
@@ -91,19 +92,71 @@ module.exports = {
   },
   login: async (req, res) => {
     try {
-      const { email, username, nohp } = req.body;
-      const checkUserData = await authModel.checkUserData(
-        username,
-        email,
-        nohp
+      const { email, password } = req.body;
+      const checkUserData = await authModel.checkUserData(null, email);
+
+      if (checkUserData.length < 1) {
+        return helperWrapper.response(res, 400, `email not registred`, null);
+      }
+
+      //checking accountStatus isActive ?
+      if (checkUserData[0].accountStatus !== "active") {
+        return helperWrapper.response(
+          res,
+          400,
+          `check your email for account acticvation`,
+          null
+        );
+      }
+
+      //compare password
+      const validPass = await bcrypt.compare(
+        password,
+        checkUserData[0].password
+      );
+      if (!validPass) {
+        return helperWrapper.response(res, 400, `wrong password`);
+      }
+
+      //declare payload
+      const payload = checkUserData[0];
+      delete payload.password;
+
+      //generate token
+      const token = jwt.sign({ ...payload }, process.env.JWT_SECRETE_KEY, {
+        expiresIn: "1h",
+      });
+
+      //generate refresh token
+      const refreshToken = jwt.sign(
+        { ...payload },
+        process.env.JWT_SECRETE_KEY,
+        { expiresIn: "24h" }
       );
 
-      // if (checkUserData.length < 1) {
-      //   return helperWrapper.response(res, 400, `email not registred`, null);
-      // }
+      return helperWrapper.response(res, 200, `success login`, {
+        username: payload.username,
+        token,
+        refreshToken,
+      });
+    } catch (error) {
+      return helperWrapper.response(
+        res,
+        400,
+        `bad request ${error.message}`,
+        null
+      );
+    }
+  },
+  logout: async (req, res) => {
+    try {
+      let token = req.headers.authorization;
 
-      console.log(checkUserData);
-      return helperWrapper.response(res, 200, `success login`, checkUserData);
+      token = token.split(" ")[1];
+
+      //redis ================================================
+
+      return helperWrapper.response(res, 200, `success logout`, null);
     } catch (error) {
       return helperWrapper.response(
         res,
--- src/modules/auth/authRoutes.js
+++ src/modules/auth/authRoutes.js
@@ -5,6 +5,7 @@ const authController = require("./authController");
 
 Router.post("/register", authController.registerWorker);
 Router.post("/login", authController.login);
+Router.post("/logout", authController.logout);
 Router.get("/activate-account/:username", authController.accountActivation);
 
 module.exports = Router;
